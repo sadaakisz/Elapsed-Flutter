f9296f90a03622443a3154b3e3af4cc65d560530
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-08-01 21:30:32 -0500

    Added color palette from Figma project.

---
+++ elapsed_flutter/lib/colors/elapsed_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class EColors {
+  static const Color black = const Color(0xFF1A1A1A);
+  static const Color red = const Color(0xFFDE2D4E);
+  static const Color green = const Color(0xFFC0E8A1);
+  static const Color aqua = const Color(0xFF2DDEBE);
+  static const Color light_blue = const Color(0xFF5CFFD8);
+  static const Color purple = const Color(0xFF8F7CFF);
+}
