3bcc888f684dabec5ce8a40992fb9621a127039d
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-08-03 21:07:33 -0500

    Created home widget.

---
+++ elapsed_flutter/lib/pages/home.dart
@@ -0,0 +1,21 @@
+import 'package:elapsed_flutter/colors/elapsed_colors.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: EColors.black,
+      body: Center(
+        child: Text('elapsed.'),
+      ),
+    );
+  }
+}
