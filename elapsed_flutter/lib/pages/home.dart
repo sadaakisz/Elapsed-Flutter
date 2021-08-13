ed477f196e2f2fed3dabfa901175dfbfae27fca0
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-08-12 19:30:43 -0500

    Updated tutorial and empty logic and commented mock routines.

--- elapsed_flutter/lib/pages/home.dart
+++ elapsed_flutter/lib/pages/home.dart
@@ -1,10 +1,12 @@
 import 'package:elapsed_flutter/colors/elapsed_colors.dart';
 import 'package:elapsed_flutter/models/custom_routine.dart';
 import 'package:elapsed_flutter/widgets/elapsed_title.dart';
+import 'package:elapsed_flutter/widgets/empty_start.dart';
 import 'package:elapsed_flutter/widgets/home_carousel.dart';
 import 'package:elapsed_flutter/widgets/navbar.dart';
 import 'package:elapsed_flutter/widgets/tutorial_start.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -15,12 +17,37 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<CustomRoutine> customRoutines = [
-    CustomRoutine(name: 'Pomodoro 1', timerTime: 25, breakTime: 5),
+    /*CustomRoutine(name: 'Pomodoro 1', timerTime: 25, breakTime: 5),
     CustomRoutine(name: 'Second Pomodoro', timerTime: 30, breakTime: 3),
     CustomRoutine(name: 'Third Pomo', timerTime: 30, breakTime: 3),
     CustomRoutine(name: '4 Pomodoro', timerTime: 30, breakTime: 3),
-    CustomRoutine(name: 'Go Pomodoro', timerTime: 30, breakTime: 3),
+    CustomRoutine(name: 'Go Pomodoro', timerTime: 30, breakTime: 3),*/
   ];
+
+  String tutorialDismissed = 'NOT DISMISSED';
+
+  _getTutorialState() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      tutorialDismissed = (prefs.containsKey('tutorialDismissed')
+          ? prefs.getString('tutorialDismissed')
+          : 'NOT DISMISSED')!;
+    });
+  }
+
+  void _dismissTutorial() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      tutorialDismissed = 'DISMISSED';
+    });
+    await prefs.setString('tutorialDismissed', tutorialDismissed);
+  }
+
+  @override
+  void initState() {
+    _getTutorialState();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +59,11 @@ class _HomeState extends State<Home> {
           children: [
             ElapsedTitle(),
             customRoutines.length == 0
-                ? TutorialStart()
+                ? tutorialDismissed == 'NOT DISMISSED'
+                    ? TutorialStart(
+                        onDismiss: _dismissTutorial,
+                      )
+                    : EmptyStart()
                 : HomeCarousel(
                     customRoutines: customRoutines,
                     onDelete: _deleteRoutine,
