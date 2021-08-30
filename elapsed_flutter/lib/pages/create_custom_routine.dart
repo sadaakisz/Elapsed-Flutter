97064544a28838556ee9726e5373446df1c393b7
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-08-30 09:50:47 -0500

    Created baseline for CreateCustomRoutine.

---
+++ elapsed_flutter/lib/pages/create_custom_routine.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateCustomRoutine extends StatefulWidget {
+  const CreateCustomRoutine({Key? key}) : super(key: key);
+
+  @override
+  _CreateCustomRoutineState createState() => _CreateCustomRoutineState();
+}
+
+class _CreateCustomRoutineState extends State<CreateCustomRoutine> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
