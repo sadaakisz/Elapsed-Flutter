ce80b9ba7f9615ad617a5ec58e147bf081dbf99f
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-08-30 20:55:32 -0500

    Established baseline for CreateCustomRoutine.

--- elapsed_flutter/lib/pages/create_custom_routine.dart
+++ elapsed_flutter/lib/pages/create_custom_routine.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
+import 'package:elapsed_flutter/colors/elapsed_colors.dart';
+import 'package:elapsed_flutter/utils/color_utils.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CreateCustomRoutine extends StatefulWidget {
   const CreateCustomRoutine({Key? key}) : super(key: key);
@@ -8,8 +13,51 @@ class CreateCustomRoutine extends StatefulWidget {
 }
 
 class _CreateCustomRoutineState extends State<CreateCustomRoutine> {
+  late SharedPreferences prefs;
+
+  Color backgroundColor = EColors.black;
+  String backgroundPath = '';
+
+  Future<void> _initializeSharedPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+    setState(() {
+      backgroundColor = (prefs.getString('backgroundColor')!.toColorFromHex());
+      backgroundPath = prefs.getString('backgroundImage')!;
+    });
+  }
+
+  @override
+  void initState() {
+    _initializeSharedPrefs();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: backgroundColor,
+      body: Stack(
+        children: <Widget>[
+          backgroundPath != ''
+              ? Positioned.fill(
+                  child: Opacity(
+                  opacity: 0.5,
+                  child: Image.file(
+                    File(backgroundPath),
+                    fit: BoxFit.cover,
+                  ),
+                ))
+              : SizedBox(),
+          SafeArea(
+            child: Stack(
+              alignment: Alignment.center,
+              children: <Widget>[],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
