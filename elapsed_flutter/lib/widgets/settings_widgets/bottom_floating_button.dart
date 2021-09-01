f9f1874fa2ca74a071dfb772a981ec7e9495ad19
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-08-31 22:47:02 -0500

    Implemented optional back button.

--- elapsed_flutter/lib/widgets/settings_widgets/bottom_floating_button.dart
+++ elapsed_flutter/lib/widgets/settings_widgets/bottom_floating_button.dart
@@ -1,3 +1,5 @@
+import 'package:elapsed_flutter/colors/elapsed_colors.dart';
+import 'package:elapsed_flutter/utils/custom_navigator.dart';
 import 'package:flutter/material.dart';
 
 class BottomFloatingButton extends StatelessWidget {
@@ -5,12 +7,14 @@ class BottomFloatingButton extends StatelessWidget {
   final double width;
   final Color foregroundColor;
   final VoidCallback onTap;
+  final bool backButton;
   const BottomFloatingButton({
     Key? key,
     required this.child,
     required this.width,
     this.foregroundColor = const Color(0xFF424242),
     required this.onTap,
+    this.backButton = false,
   }) : super(key: key);
 
   @override
@@ -18,20 +22,44 @@ class BottomFloatingButton extends StatelessWidget {
     return Positioned(
       bottom: width / 13,
       width: width * 0.86,
-      child: GestureDetector(
-        child: Container(
-          height: width / 8,
-          decoration: BoxDecoration(
-            color: foregroundColor,
-            borderRadius: BorderRadius.circular(3),
+      child: Row(
+        children: [
+          Expanded(
+            child: GestureDetector(
+              child: Container(
+                height: width / 8,
+                decoration: BoxDecoration(
+                  color: foregroundColor,
+                  borderRadius: BorderRadius.circular(3),
+                ),
+                child: Center(
+                  child: child,
+                ),
+              ),
+              onTap: () {
+                onTap();
+              },
+            ),
           ),
-          child: Center(
-            child: child,
-          ),
-        ),
-        onTap: () {
-          onTap();
-        },
+          backButton ? SizedBox(width: width / 20) : SizedBox(),
+          backButton
+              ? GestureDetector(
+                  onTap: () => navPop(context),
+                  child: Container(
+                    width: width / 8,
+                    height: width / 8,
+                    decoration: BoxDecoration(
+                      color: foregroundColor,
+                      borderRadius: BorderRadius.circular(3),
+                    ),
+                    child: Icon(
+                      Icons.clear,
+                      color: EColors.red,
+                    ),
+                  ),
+                )
+              : SizedBox(),
+        ],
       ),
     );
   }
