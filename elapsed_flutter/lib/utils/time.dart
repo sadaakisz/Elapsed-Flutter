d915c5ab1163005a88a70afbe8ea2d2c87f69d63
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-09-01 23:39:06 -0500

    Implemented notification sound play when timer and break time is over.

--- elapsed_flutter/lib/utils/time.dart
+++ elapsed_flutter/lib/utils/time.dart
@@ -1,15 +1,35 @@
 import 'dart:async';
+
 import 'package:elapsed_flutter/models/time_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
 
 class Time {
-  void tickTimerTime(Timer timer, TimeModel timerTime, TimeModel breakTime,
-      bool timerTimeTurn, Function resetTimer, BuildContext context) {
+  bool playedTimerFinished = false;
+  bool playedBreakFinished = false;
+  void tickTimerTime(
+      Timer timer,
+      TimeModel timerTime,
+      TimeModel breakTime,
+      bool timerTimeTurn,
+      Function resetTimer,
+      double volume,
+      BuildContext context) {
     if (timerTime.actualSeconds! < 1) {
       if (timerTime.actualMinutes == 0) {
         timerTimeTurn = false;
-        tickBreakTime(
-            timer, timerTime, breakTime, timerTimeTurn, resetTimer, context);
+        if (!playedTimerFinished) {
+          FlutterRingtonePlayer.play(
+            android: AndroidSounds.notification,
+            ios: IosSounds.glass,
+            looping: false, // Android only - API >= 28
+            volume: volume, // Android only - API >= 28
+            asAlarm: false, // Android only - all APIs
+          );
+          playedTimerFinished = true;
+        }
+        tickBreakTime(timer, timerTime, breakTime, timerTimeTurn, resetTimer,
+            volume, context);
       } else {
         timerTime.actualMinutes = (timerTime.actualMinutes! - 1);
         timerTime.actualSeconds = 59;
@@ -20,12 +40,28 @@ class Time {
     timerTime.updateDisplay();
   }
 
-  void tickBreakTime(Timer timer, TimeModel timerTime, TimeModel breakTime,
-      bool timerTimeTurn, Function resetTimer, BuildContext context) {
+  void tickBreakTime(
+      Timer timer,
+      TimeModel timerTime,
+      TimeModel breakTime,
+      bool timerTimeTurn,
+      Function resetTimer,
+      double volume,
+      BuildContext context) {
     if (breakTime.actualSeconds! < 1) {
       if (breakTime.actualMinutes == 0) {
         timer.cancel();
         resetTimer();
+        if (!playedBreakFinished) {
+          FlutterRingtonePlayer.play(
+            android: AndroidSounds.notification,
+            ios: IosSounds.glass,
+            looping: false, // Android only - API >= 28
+            volume: volume, // Android only - API >= 28
+            asAlarm: false, // Android only - all APIs
+          );
+          playedBreakFinished = true;
+        }
         Navigator.pop(context);
       } else {
         breakTime.actualMinutes = breakTime.actualMinutes! - 1;
