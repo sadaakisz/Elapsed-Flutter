0afdc5d323b0fd80c7e455e41552be74552ead4b
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-09-02 12:12:10 -0500

    Made script to move the generated APK to root as well.

--- elapsed_flutter/GenerateReleaseAPK.bat
+++ elapsed_flutter/GenerateReleaseAPK.bat
@@ -1,1 +1,4 @@
-flutter build apk --split-per-abi --no-shrink
+CALL flutter build apk --split-per-abi --no-shrink
+move /Y D:\Libraries\Documents\PersonalProjects\Elapsed-Flutter\elapsed_flutter\build\app\outputs\apk\release\app-armeabi-v7a-release.apk D:\Libraries\Documents\PersonalProjects\Elapsed-Flutter\elapsed_flutter
+:End
+PAUSE
