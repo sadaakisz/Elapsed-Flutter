3ba4d2935f13a421b8918961c4965e1bb64614d7
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-09-01 22:16:12 -0500

    Added optional param blurStrength.

--- elapsed_flutter/lib/widgets/settings_widgets/blur_container.dart
+++ elapsed_flutter/lib/widgets/settings_widgets/blur_container.dart
@@ -4,14 +4,16 @@ import 'package:flutter/material.dart';
 
 class BlurContainer extends StatelessWidget {
   final double borderRadius;
-  const BlurContainer({Key? key, this.borderRadius = 5}) : super(key: key);
+  final double blurStrength;
+  const BlurContainer({Key? key, this.borderRadius = 5, this.blurStrength = 3})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(borderRadius),
       child: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+        filter: ImageFilter.blur(sigmaX: blurStrength, sigmaY: blurStrength),
         child: Container(
           color: Colors.white.withOpacity(0.07),
         ),
