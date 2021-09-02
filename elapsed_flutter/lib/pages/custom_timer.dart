3e117c67817aa35376b41842c5e0af5b788db6de
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-09-01 23:38:41 -0500

    Using volume param from time class.

--- elapsed_flutter/lib/pages/custom_timer.dart
+++ elapsed_flutter/lib/pages/custom_timer.dart
@@ -32,6 +32,8 @@ class _CustomTimerPageState extends State<CustomTimerPage> {
   int get index => widget.index;
   Timer? _timer;
   CustomRoutine get customRoutine => widget.customRoutine;
+  double get volume =>
+      double.parse(widget.customRoutine.notificationVolume.toString());
   String get backgroundPath => widget.customRoutine.background;
 
   TimeModel timerTime = new TimeModel(minutes: 10, seconds: 0);
@@ -91,10 +93,10 @@ class _CustomTimerPageState extends State<CustomTimerPage> {
         (Timer timer) => setState(() {
               if (timerTimeTurn) {
                 time.tickTimerTime(timer, timerTime, breakTime, timerTimeTurn,
-                    resetTimer, context);
+                    resetTimer, volume, context);
               } else {
                 time.tickBreakTime(timer, timerTime, breakTime, timerTimeTurn,
-                    resetTimer, context);
+                    resetTimer, volume, context);
               }
             }));
   }
