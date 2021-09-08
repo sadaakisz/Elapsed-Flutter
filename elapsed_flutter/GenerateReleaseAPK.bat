f51cef056d9dcd77bb9698b92beaba1beb4bb48d
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-09-07 22:10:30 -0500

    Using relative path.

--- elapsed_flutter/GenerateReleaseAPK.bat
+++ elapsed_flutter/GenerateReleaseAPK.bat
@@ -1,5 +1,5 @@
 CALL flutter build apk --split-per-abi --no-shrink
-move /Y .\build\app\outputs\apk\release\app-armeabi-v7a-release.apk D:\Libraries\Documents\PersonalProjects\Elapsed-Flutter\elapsed_flutter
+move /Y .\build\app\outputs\apk\release\app-armeabi-v7a-release.apk .
 start .
 :End
 PAUSE
